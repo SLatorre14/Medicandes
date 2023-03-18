a1dd36d143d5a272c79c735420409ccf3f4272c1
NICOMB22 <nicolasmaldonadob@gmail.com>
2023-03-17 21:47:28 -0500

    pk

--- historiaclinica/urls.py
+++ historiaclinica/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.historiaclinica_view, name='historiaclinica_view'),
-    path('<numHistoriaClinica>', views.historiaclinica_view, name='historiaclinica_view')
+    path('', views.historiaclinica_view, name='historiaclinica_login'),
+    path('<numHistoriaClinica>', views.historiaclinica_view, name='historiaclinica_login')
 ]
