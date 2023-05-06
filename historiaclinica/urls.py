39fe8bb070c3d32027218db3edcd845316437242
NicoleMurillo <n.murillof@uniandes.edu.co>
2023-05-05 19:21:16 -0500

    urls

--- historiaclinica/urls.py
+++ historiaclinica/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import path
 
 urlpatterns = [
     path('', views.historiasclinicas_view, name='historiaclinica_view'),
-    path('<int:pk>', views.historiaclinica_view, name='historiaclinica_login')
+    path('<int:pk>', views.historiaclinica_view, name='historiaclinica_login'),
+    path('historiaclinicaCreate/', csrf_exempt(views.create_historiaclinica), name='historiaclinicaCreate'),
 ]
